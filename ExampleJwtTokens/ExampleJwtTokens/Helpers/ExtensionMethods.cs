59ae32577e2fd73b8aa6d3ae1e8c7ea997890cda
monicar <monicar@proarch.com>
2020-03-27 12:15:00 +0530

    backend

---
+++ ExampleJwtTokens/ExampleJwtTokens/Helpers/ExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using ExampleJwtTokens.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleJwtTokens.Helpers
+{
+    public static class ExtensionMethods
+    {
+
+        public static IEnumerable<User> WithoutPasswords(this IEnumerable<User> users)
+        {
+            return users.Select(x => x.WithoutPassword());
+        }
+
+        public static User WithoutPassword(this User user)
+        {
+            user.Password = null;
+            return user;
+        }
+    }
+}
